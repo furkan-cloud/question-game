06b8f55ddd4798bb133110857fc2e156ad5ad847
furkan-cloud <firstinfirstout2020@gmail.com>
2020-11-08 20:41:42 +0300

    redux components added intro page completed

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -6,17 +6,38 @@ import {
   TouchableOpacity,
   Animated,
 } from 'react-native';
+import {CountdownCircleTimer} from 'react-native-countdown-circle-timer';
+import {useDispatch} from 'react-redux';
+import {introPage} from './styles';
+import axios from 'axios';
 import {CategorySelectModal} from '../components';
 
-import {introPage} from './styles';
-
 const Intro = (props) => {
+  const [counterFlag, setCounterFlag] = useState(false);
   const [modalFlag, setModalFlag] = useState(false);
+  const dispatch = useDispatch();
 
   const startGame = (selectedCategory) => {
-    console.log(selectedCategory);
-    setModalFlag(false)
-  }
+    // axios.get(`https://opentdb.com/api.php?amount=10&category=${selectedCategory.id}&type=boolean`)
+    axios
+      .get(`https://opentdb.com/api.php?`, {
+        params: {
+          amount: 10,
+          category: selectedCategory.id,
+          type: 'boolean',
+        },
+      })
+      .then((response) => {
+        const {
+          data: {results: questions},
+        } = response;
+
+        dispatch({type: 'SET_QUESTIONS', payload: {questions}});
+      });
+
+    setModalFlag(false);
+    setCounterFlag(true);
+  };
 
   return (
     <SafeAreaView style={{flex: 1}}>
@@ -24,6 +45,25 @@ const Intro = (props) => {
         <View style={introPage.container}>
           <Text style={introPage.bannerText}>Trivia!</Text>
         </View>
+
+        <View style={{backgroundColor: '#3949ab', alignItems: 'center'}}>
+          <CountdownCircleTimer
+            isPlaying={counterFlag}
+            duration={5}
+            onComplete={() => props.navigation.navigate('Questions')}
+            colors={[
+              ['#fff176', 0.4],
+              ['#ba68c8', 0.4],
+              ['#ff8a65', 0.2],
+            ]}>
+            {({remainingTime, animatedColor}) => (
+              <Animated.Text style={{fontSize: 60, color: animatedColor}}>
+                {remainingTime}
+              </Animated.Text>
+            )}
+          </CountdownCircleTimer>
+        </View>
+
         <View style={introPage.container}>
           <TouchableOpacity
             style={introPage.buttonContainer}
@@ -32,7 +72,7 @@ const Intro = (props) => {
           </TouchableOpacity>
         </View>
 
-        <CategorySelectModal 
+        <CategorySelectModal
           visibility={modalFlag}
           onClose={() => setModalFlag(false)}
           onCategorySelect={startGame}
